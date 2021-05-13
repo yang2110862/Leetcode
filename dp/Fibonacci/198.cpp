1156627c112c472ec6405f51c912bac507bfd648
yang2110862 <1563782600@qq.com>
2021-05-13 19:17:38 +0800

    5/13

---
+++ dp/Fibonacci/198.cpp
@@ -0,0 +1,21 @@
+#include "header.h"
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int len = nums.size();
+        if (len == 1) return nums[0];
+        else if (len == 2) return max(nums[0], nums[1]);
+        else return max(rob(nums, 0, len - 2), rob(nums, 1, len - 1));
+    }
+private:
+    int rob(vector<int>& nums, int beg, int end) {
+        int first = nums[beg], second = max(nums[beg], nums[beg + 1]);
+        for (int i = start + 2; i <= end; ++i) {
+            int temp = second;
+            second = max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+};

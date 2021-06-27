b31655dea84e3b68e572b71eb8f56365680aa281
yang2110862 <1563782600@qq.com>
2021-06-27 22:48:07 +0800

    6/27

---
+++ _daily_topic/909.cpp
@@ -0,0 +1,74 @@
+#include "header.h"
+
+class Solution {
+private:
+    unordered_set<int> visited;
+    vector<int> convert2dimTo1dim(vector<vector<int>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        int len = m * n;
+        vector<int> arr;
+        int leftToRight = true;
+        for (int i = m - 1; i >= 0; --i) {
+            if (leftToRight) {
+                leftToRight = false;
+                for (int j = 0; j < n; ++j) {
+                    arr.push_back(board[i][j]);
+                }
+            } else {
+                leftToRight = true;
+                for (int j = n - 1; j >= 0; --j) {
+                    arr.push_back(board[i][j]);
+                }
+            }
+        }
+        return arr;
+    }
+    vector<int> canReach(int num, vector<int> arr) {
+        int len = arr.size();
+        vector<int> ret;
+        for (int i = 1; i <= 6; ++i) {
+            if ((num + i) <= len) {
+                // if (visited.count(num + i)) continue;
+                if (arr[num + i - 1] != -1) {
+                    // if (visited.count(arr[num + i - 1])) continue;
+                    ret.push_back(arr[num + i - 1]);
+                }  else ret.push_back(num + i);
+            }
+        }
+        return ret;
+    }
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        int len = m * n;
+        vector<int> arr(len);
+        arr = convert2dimTo1dim(board);
+        queue<int> que;
+        que.push(1);
+        visited.insert(1);
+        int step = 0;
+        while (!que.empty()) {
+            int num = que.size();
+            ++step;
+            for (int i = 0; i < num; ++i) {
+                int loc = que.front();
+                que.pop();
+                for (auto x : canReach(loc, arr)) {
+                    if (visited.count(x)) continue;
+                    if (x == len) return step;
+                    que.push(x);
+                    visited.insert(x);
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution test;
+    vector<vector<int>> board{{1,1,-1}, {1,1,1}, {-1,1,1}};
+    cout << test.snakesAndLadders(board);
+}

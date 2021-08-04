e0ce340272f94dc960e101d99d476d823ff2f2ff
yang2110862 <1563782600@qq.com>
2021-08-04 15:04:22 +0800

    8/4

---
+++ _daily_topic/math/611.cpp
@@ -0,0 +1,39 @@
+#include "header.h"
+//有效三角形的个数
+class Solution1 {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int upper = lower_bound(nums.begin(), nums.end(), nums[i] + nums[j]) - nums.begin();
+                int num = upper - 1 - j;
+                if (num > 0) {
+                    ans += num;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; +i) {
+            int k = i + 1;
+            for (int j = i + 1; j < n; ++j) {
+                while (k < n && nums[k] < nums[i] + nums[j]) {
+                    ++k;
+                }
+                ans += max((k - 1 - j), 0);
+            }
+        }
+        return ans;
+    }
+};
--- test.cpp
+++ test.cpp
@@ -1,41 +1,6 @@
 #include "header.h"
-class Solution1 {
-private:
-    static constexpr int UNCOLORED = 0;
-    static constexpr int RED = 1;
-    static constexpr int GREEN = 2;
-public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n, 0);
-        for (int i = 0; i < n; ++i) {
-            if (color[i] == UNCOLORED) {
-                queue<int> q;
-                q.push(i);
-                color[i] = RED;
-                while (!q.empty()) {
-                    int node = q.front();
-                    int cNei = (color[node] == RED ? GREEN : RED);
-                    q.pop();
-                    for (int neighbor: graph[node]) {
-                        if (color[neighbor] == UNCOLORED) {
-                            q.push(neighbor);
-                            color[neighbor] = cNei;
-                        }
-                        else if (color[neighbor] != cNei) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    }
-};
 
 int main() {
-    Solution1 test;
-    vector<vector<int>> graph{{1,2,3}, {0,2}, {0,1,3}, {0,2}};
-    auto x = test.isBipartite(graph);
-    cout << "hehe";
+    vector<int> arr{1,2,3,4,5};
+    cout << lower_bound(arr.begin(), arr.end(), 3) - arr.begin();
 }

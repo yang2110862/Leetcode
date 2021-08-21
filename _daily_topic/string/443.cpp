2480faa6ab7e18d369630b8488780b357b5efa65
yang2110862 <1563782600@qq.com>
2021-08-21 15:34:07 +0800

    8/21

---
+++ _daily_topic/string/443.cpp
@@ -0,0 +1,26 @@
+#include "header.h"
+//压缩字符串
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int write = 0, left = 0;
+        int n = chars.size();
+        for (int read = 0; read < n; ++read) {
+            if (read == n - 1 || chars[read] != chars[read + 1]) {
+                chars[write++] = chars[read];
+                int num = read - left + 1;
+                if (num > 1) {
+                    int anchor = write;
+                    while (num > 0) {
+                        chars[write++] = num % 10 + '0';
+                        num /= 10;
+                    }
+                    reverse(&chars[anchor], &chars[write]);
+                }
+                left = read + 1;
+            }
+        }
+        return write;
+    }
+};
--- test.cpp
+++ test.cpp
@@ -3,8 +3,7 @@
 
 
 int main() {
-    int n = 5;
-    int x = -6;
-    int ans = floor(0.3);
-    cout << ans;
+    string s = "abcd";
+    reverse(&s[0], &s[4]);
+    cout << s;
 }
